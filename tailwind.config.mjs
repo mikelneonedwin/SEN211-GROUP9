9c4871ece724b5b8c5c3d538f95eb99197354c4e
Mikel Edwin <michealanthonyxxx@gmail.com>
2025-01-08 01:23:46 +0100

    Init

---
+++ tailwind.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FF9500",
+      },
+      fontFamily: {
+        afacad: ["Afacad", "serif"],
+        "open-sans": ["Open Sans", "serif"],
+        inter: ["Inter", "serif"],
+      },
+    },
+  },
+  plugins: [],
+  corePlugins: {
+    backgroundOpacity: false,
+    textOpacity: false,
+    borderOpacity: false,
+  },
+};
